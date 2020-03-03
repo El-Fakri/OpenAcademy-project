800c7e1d107afb8d457bbcaabe278dcc3b9121f9
El-Fakri <el.fakri.el.mahdi@gmail.com>
2020-03-03 11:46:06 +0100

    [ADD]

--- openacademy/models/Partner.py
+++ openacademy/models/Partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models,_
+from odoo import fields, models, exceptions,_
 from odoo.tools.misc import formatLang, format_date, get_lang
 
 
@@ -49,12 +49,14 @@ class Partner(models.Model):
             unit_price = session.price_for_one_hour
 
         if test == 0:
-            return {
-                'warning': {
-                    'title': "No Session",
-                    'message': "No Session to put Invoice For this Costumer",
-                }
-            }
+            print("No Session to put Invoice For this Costumer")
+            raise exceptions.ValidationError("No Session to put Invoice For this Costumer")
+            # return {
+            #     'warning': {
+            #         'title': "No Session",
+            #         'message': "No Session to put Invoice For this Costumer",
+            #     }
+            # }
 
         line2 = {
             "name": name,
@@ -107,39 +109,39 @@ class Partner(models.Model):
         action['context'] = context
         return action
 
-    # def print_invoices(self):
-    #     invoice = self.env['account.move'].search([('id', '=', self.id_of_latest_invoice)])
-    #
-    #
-    #     # return self.env.ref('account.report_invoice').report_action(self)
-    #     self.ensure_one()
-    #     template = self.env.ref('account.email_template_edi_invoice', raise_if_not_found=False)
-    #     lang = get_lang(self.env)
-    #     if template and template.lang:
-    #         lang = template._render_template(template.lang, 'account.move', self.id)
-    #     else:
-    #         lang = lang.code
-    #     compose_form = self.env.ref('account.account_invoice_send_wizard_form', raise_if_not_found=False)
-    #     ctx = dict(
-    #         default_model='account.move',
-    #         default_res_id=self.id,
-    #         default_use_template=bool(template),
-    #         # default_template_id=template and template.id or False,
-    #         default_template_id=template and template.id or False,
-    #         default_composition_mode='comment',
-    #         mark_invoice_as_sent=True,
-    #         custom_layout="mail.mail_notification_paynow",
-    #         # model_description=self.with_context(lang=lang).type_name,
-    #         force_email=True
-    #     )
-    #     return {
-    #         'name': _('Send Invoice'),
-    #         'type': 'ir.actions.act_window',
-    #         'view_type': 'form',
-    #         'view_mode': 'form',
-    #         'res_model': 'account.invoice.send',
-    #         'views': [(compose_form.id, 'form')],
-    #         'view_id': compose_form.id,
-    #         'target': 'new',
-    #         'context': ctx,
-    #     }
+    def print_invoices(self):
+        invoice = self.env['account.move'].search([('id', '=', self.id_of_latest_invoice)])
+
+
+        # return self.env.ref('account.report_invoice').report_action(self)
+        # self.ensure_one()
+        # template = self.env.ref('account.email_template_edi_invoice', raise_if_not_found=False)
+        # lang = get_lang(self.env)
+        # if template and template.lang:
+        #     lang = template._render_template(template.lang, 'account.move', self.id)
+        # else:
+        #     lang = lang.code
+        # compose_form = self.env.ref('account.account_invoice_send_wizard_form', raise_if_not_found=False)
+        # ctx = dict(
+        #     default_model='account.move',
+        #     default_res_id=self.id,
+        #     default_use_template=bool(template),
+        #     # default_template_id=template and template.id or False,
+        #     default_template_id=template and template.id or False,
+        #     default_composition_mode='comment',
+        #     mark_invoice_as_sent=True,
+        #     custom_layout="mail.mail_notification_paynow",
+        #     # model_description=self.with_context(lang=lang).type_name,
+        #     force_email=True
+        # )
+        # return {
+        #     'name': _('Send Invoice'),
+        #     'type': 'ir.actions.act_window',
+        #     'view_type': 'form',
+        #     'view_mode': 'form',
+        #     'res_model': 'account.invoice.send',
+        #     'views': [(compose_form.id, 'form')],
+        #     'view_id': compose_form.id,
+        #     'target': 'new',
+        #     'context': ctx,
+        # }
